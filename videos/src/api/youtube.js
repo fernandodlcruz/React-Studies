231da2bcff8b936d96b5bb3573cbc74c99ede853
Fernando de La Cruz <fernando.dlcruz@gmail.com>
2020-01-21 11:33:17 -0800

    removed sensitivy data

--- videos/src/api/youtube.js
+++ videos/src/api/youtube.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const KEY = 'AIzaSyAFUUkUTdo7K29GcNATIqu1dqSP1la3HEM';
+const KEY = '';
 
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
